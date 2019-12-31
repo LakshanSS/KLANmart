00ff46e04b28ea78662576f134410365db65f395
LakshanSS <lakshan230897@gmail.com>
2019-12-31 20:52:38 +0530

    Refactor common js

--- js/common.js
+++ js/common.js
@@ -2,8 +2,6 @@ function startJourney() {
     document.location.href = "./introductoryPage01.html";
 }
 
-
-
 // navigation functions
 function favourite() {
     document.location.href = "./favourites.html";
@@ -40,22 +38,3 @@ function goToFeedback() {
 function goToAR() {
     document.location.href = "./arForProduct.html";
 }
-
-
-{/* <div class="post">
-    <img src="../assets/images/favourite/tshirt1-blue.png" alt="" srcset="" class="post-image">
-        <div class="post-preview">
-            <h4>2019 Spring Brand Clothing Men's Long Sleeve Round Neck T-Shirt</h4>
-            <p class="store-detail"><strong>Store</strong> Trendy Fashion Store</p>
-            <h3 class="price-tag">LKR 2199.00</h3>
-            <!-- <i class="fas fa-heart heart-icon"></i> -->
-                        </div>
-                    </div>
-    <div class="post">
-        <img src="../assets/images/favourite/image2.jpeg" alt="" srcset="" class="post-image">
-            <div class="post-preview">
-                <h4>Hayiou gt1 tws water proof Finger print Touch Blutooth Earphones</h4>
-                <p class="store-detail"><strong>Store</strong> 3C Global Tradings</p>
-                <h3 class="price-tag">LKR 1500.00</h3>
-            </div>
-                    </div> */}
